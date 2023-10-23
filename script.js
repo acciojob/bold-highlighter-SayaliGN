9995553d95f117856c8f0bfbf96fcfa3df26d448
Sayali Nikam <137673498+SayaliGN@users.noreply.github.com>
2023-10-23 13:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-function highlight() {
-    //Write your code here
-
-
+var bold_Items;
+window.onload = getBold_items();
+ 
+// Collect all <strong> tags
+function getBold_items() 
+{
+  bold_Items = document.getElementsByTagName('strong'); 
+}
+// iterate all bold tags and change color  
+function highlight() 
+{
+   for (var i=0; i<bold_Items.length; i++)
+   {                                                    
+    bold_Items[i].style.color = "green";
+    }
 }
 
 
 function return_normal() {
     //Write your code here
 
-    
+     for (var i=0; i<bold_Items.length; i++) 
+  {
+       bold_Items[i].style.color = "black";
+  }
 }
